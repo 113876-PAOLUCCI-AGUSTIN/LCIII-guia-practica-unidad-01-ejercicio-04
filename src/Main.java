2bda6e81952f30bb5226d5b52e299c82271b44b6
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-20 22:26:23 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/*
+ * LC_III-guía-práctica-unidad-01-ejercicio-04
+ * 4. Ingresar nombre y altura de dos personas, informar el nombre de la más alta.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
